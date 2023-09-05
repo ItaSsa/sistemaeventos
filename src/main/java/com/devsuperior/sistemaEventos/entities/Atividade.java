46fc5df8db907c64e370e6c084e31dfebb2f61b9
Itaira <itairassa@gmail.com>
2023-09-05 13:03:31 +0200

    2-Entidade: bloco - Relacionamentos um-um e um-muitos

--- src/main/java/com/devsuperior/sistemaEventos/entities/Atividade.java
+++ src/main/java/com/devsuperior/sistemaEventos/entities/Atividade.java
@@ -1,6 +1,10 @@
 package com.devsuperior.sistemaEventos.entities;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,7 +12,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -28,6 +34,9 @@ public class Atividade {
 	@ManyToOne
 	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
+
+	@OneToMany(mappedBy = "atividade")
+	private List<Bloco> blocos = new ArrayList<>();
 	
 	public Atividade() {
 
