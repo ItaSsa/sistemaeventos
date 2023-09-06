b503ffb89a2aabf171c39695e7b61365f6b3d950
Itaira <itairassa@gmail.com>
2023-09-06 11:13:44 +0200

    5- Seeding entidade Participante

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,3 +9,8 @@ INSERT INTO tb_atividade (categoria_id,nome,descricao,preco) VALUES (2,'Oficina 
 INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (1,TIMESTAMP WITH TIME ZONE '2017-09-25T08:00:00Z',TIMESTAMP WITH TIME ZONE '2017-09-25T11:00:00Z')
 INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (2,TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z',TIMESTAMP WITH TIME ZONE '2017-09-25T18:00:00Z')
 INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (2,TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z',TIMESTAMP WITH TIME ZONE '2017-09-26T11:00:00Z')
+
+INSERT INTO tb_participante(email, name) VALUES ('Jose Silva','jose@gmail.com');
+INSERT INTO tb_participante(email, name) VALUES ('Tiago Faria', 'tiago@gmail.com');
+INSERT INTO tb_participante(email, name) VALUES ('Maria do Rosario','maria@gmail.com');
+INSERT INTO tb_participante(email, name) VALUES ('Teresa Silva','teresa@gmail.com');
