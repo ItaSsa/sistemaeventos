53a1b803a479142c7c77daea8db8e30d8a320614
Itaira <itairassa@gmail.com>
2023-09-06 11:23:48 +0200

    6- Seeding entidade Participante_atividade

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,11 +6,18 @@ INSERT INTO tb_atividade (categoria_id,nome,descricao,preco) VALUES (1,'Curso de
 INSERT INTO tb_atividade (categoria_id,nome,descricao,preco) VALUES (2,'Oficina de Github','Controle de versoes de seus projetos',50.00);
 
 
-INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (1,TIMESTAMP WITH TIME ZONE '2017-09-25T08:00:00Z',TIMESTAMP WITH TIME ZONE '2017-09-25T11:00:00Z')
-INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (2,TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z',TIMESTAMP WITH TIME ZONE '2017-09-25T18:00:00Z')
-INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (2,TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z',TIMESTAMP WITH TIME ZONE '2017-09-26T11:00:00Z')
+INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (1,TIMESTAMP WITH TIME ZONE '2017-09-25T08:00:00Z',TIMESTAMP WITH TIME ZONE '2017-09-25T11:00:00Z');
+INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (2,TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z',TIMESTAMP WITH TIME ZONE '2017-09-25T18:00:00Z');
+INSERT INTO tb_bloco (atividade_id,inicio, fim) VALUES (2,TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z',TIMESTAMP WITH TIME ZONE '2017-09-26T11:00:00Z');
 
 INSERT INTO tb_participante(email, name) VALUES ('Jose Silva','jose@gmail.com');
 INSERT INTO tb_participante(email, name) VALUES ('Tiago Faria', 'tiago@gmail.com');
 INSERT INTO tb_participante(email, name) VALUES ('Maria do Rosario','maria@gmail.com');
 INSERT INTO tb_participante(email, name) VALUES ('Teresa Silva','teresa@gmail.com');
+
+INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES(1,1);
+INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES(1,2);
+INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES(2,1);
+INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES(3,1);
+INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES(3,2);
+INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES(4,2);
